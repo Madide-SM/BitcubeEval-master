d5b792862958377375cfb9a92c6237cb7c539cf1
Madide-SM <siyabongamadide5@gmail.com>
2020-07-16 19:53:33 +0200

    Add project files.

---
+++ Bitcubeeval/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bitcubeeval.MvcApplication" Language="C#" %>
---
+++ Bitcubeeval/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bitcubeeval.Startup))]
+namespace Bitcubeeval
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
