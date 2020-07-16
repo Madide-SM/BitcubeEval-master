34044ef777f9da2b5cd7db26e77b94a43c92beff
Madide-SM <siyabongamadide5@gmail.com>
2020-07-17 01:06:35 +0200

    Section1 (Added Login page)

--- Bitcubeeval/App_Start/BundleConfig.cs
+++ Bitcubeeval/App_Start/BundleConfig.cs
@@ -19,9 +19,9 @@ namespace Bitcubeeval
                 bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                             "~/Scripts/modernizr-*"));
 
-                bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                          "~/Scripts/bootstrap.js",
-                          "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
 
                 bundles.Add(new StyleBundle("~/Content/css").Include(
                           "~/Content/bootstrap.css",
